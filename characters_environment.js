982c6221757ac5b9b0eb2dbfbdde6a4ed2500aaf
oscar3176 <58105187+oscar3176@users.noreply.github.com>
2022-07-20 16:36:27 +0300

    Add files via upload

--- characters_environment.js
+++ characters_environment.js
@@ -125,7 +125,7 @@ function game(){
 function changeGameStatud(character){
   if(noseX!= "" && gameConfig.status==="start" && GameStatus == "start") {
     document.getElementById("status").innerHTML = "Game Is Loaded";
-    //world_start.play();
+    world_start.play();
     initializeCharacterStatus(mario);
     gameConfig.status= "play";
   }
@@ -222,7 +222,7 @@ function instializeInDraw(){
 function getCoins(coin,character){
   if( character.overlap(coin) && character.live && coin.get==false){
     character.coins+=1;
-    //mario_collect_coin.play();
+    mario_collect_coin.play();
     coin.get=true;
   };
 }
@@ -312,7 +312,7 @@ function manualControl(character){
 /* Movements of character */
 function jumping(character){
 	if( (noseY < 200 && character.live) || (touchIsDown&&character.live) ){
-    //mario_jump.play();
+    mario_jump.play();
 		character.velocity.y+=gameConfig.jump;
 	}
 }
@@ -363,7 +363,7 @@ function StepOnEnemy(obj1,obj2){
 		obj2.live=false;
     obj1.killing=30;
     obj1.kills++;
-    //mario_kill_enemy.play();
+    mario_kill_enemy.play();
     if(obj1.velocity.y>=gameConfig.jump*0.8){
       obj1.velocity.y=gameConfig.jump*0.8;
     }else{
@@ -378,12 +378,12 @@ function die(character){
     character.live=false;
     character.dying+=120;
     character.liveNumber--;
-    if (character.liveNumber > 0) {
-      //mario_die.play();
-    }
     character.status="dead";
     character.changeAnimation('dead');
     character.velocity.y-=2;
+    if (character.liveNumber > 0) {
+      mario_die.play();
+    }
 }
 
 // check character status and response to sprite and game status
@@ -395,7 +395,7 @@ function checkStatus(character){
   }
   if(character.live==false && character.liveNumber==0){
     gameConfig.status="gameover"
-    //game_over.play();
+    game_over.play();
   }
 
 }
